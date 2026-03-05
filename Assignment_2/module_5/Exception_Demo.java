449faee76ed1540aed17ad14bad5180d18149f7c
Siri-s25 <myself.siri2508@gmail.com>
2026-03-05 09:14:06 +0530

    Fix exception handling examples in Exception_Demo.java

--- Assignment_2/module_5/Exception_Demo.java
+++ Assignment_2/module_5/Exception_Demo.java
@@ -4,18 +4,18 @@ public class Exception_Demo {
 
         public static void main(String[] args) {
 
-            // Unchecked Exception Example
+            
             try {
                 int a = 10, b = 0;
-                int result = a / b; // ArithmeticException
+                int result = a / b; 
                 System.out.println(result);
             } catch (ArithmeticException e) {
                 System.out.println("Unchecked Exception: Division by zero");
             }
 
-            // Checked Exception Example
+           
             try {
-                FileReader file = new FileReader("test.txt"); // may cause FileNotFoundException
+                FileReader file = new FileReader("test.txt"); 
                 BufferedReader br = new BufferedReader(file);
                 System.out.println(br.readLine());
             } catch (IOException e) {
